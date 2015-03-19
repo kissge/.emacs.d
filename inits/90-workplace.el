6718ab4df22599b690090e50376e594c333696fc
kissge <mail@yo.eki.do>
2015-03-19 12:37:48 +0900

    Fix for coding conventions in my workplace

--- inits/90-workplace.el
+++ inits/90-workplace.el
@@ -1,11 +1,11 @@
 (when env-hikarie
-  ;; (add-hook 'html-mode-hook
-  ;;           (lambda ()
-  ;;             (setq tab-width 4)
-  ;;             (set (make-local-variable 'sgml-basic-offset) 4)))
-  ;; (add-hook 'js2-mode-hook
-  ;;           (lambda ()
-  ;;             (setq tab-width 4)
-  ;;             (set (make-local-variable 'sgml-basic-offset) 4)))
+  (add-hook 'html-mode-hook
+            (lambda ()
+              (setq tab-width 4)
+              (set (make-local-variable 'sgml-basic-offset) 4)))
+  (add-hook 'js2-mode-hook
+            (lambda ()
+              (setq tab-width 4)
+              (set (make-local-variable 'sgml-basic-offset) 4)))
   (setq-default tags-file-name "/eng4:TAGS")
 )
