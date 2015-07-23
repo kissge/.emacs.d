6a618787f43bc542a3b302fd29c2570c148e2c70
kissge <mail@yo.eki.do>
2015-07-23 18:01:36 +0900

    Move session.el to the last

--- inits/01-package.el
+++ inits/01-package.el
@@ -3,20 +3,6 @@
 (el-get-bundle! flymake) ; don't load this after auto-complete
 (el-get-bundle auto-complete
   (custom-set-variables `(ac-comphist-file ,(expand-file-name (concat user-emacs-directory "/.achist")))))
-(el-get-bundle! session
-  (setq history-length 10000)
-  (setq history-delete-duplicates t)
-  (custom-set-variables
-   `(session-set-file-name-exclude-regexp
-     ,(concat session-set-file-name-exclude-regexp "\\|" "[/\\]\\.scratch" "\\|" "[/\\]geben[/\\]" "\\|" "\\.loaddefs\\.el")))
-  (session-initialize)
-  (defadvice write-region (before force-quiet)
-    (ad-set-arg 4 0))
-  (add-hook 'find-file-hook
-            (lambda ()
-              (ad-activate 'write-region)
-              (session-save-session)
-              (ad-deactivate 'write-region))))
 (el-get-bundle undo-tree
   (global-undo-tree-mode 1)
   (global-set-key (kbd "C-z") 'undo-tree-undo)
