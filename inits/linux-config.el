720d70187407d8edcc57c7a62f617c2debe28572
kissge <mail@yo.eki.do>
2015-10-18 21:49:34 +0900

    Set s-SPC to toggle-input-method on Linux

--- inits/linux-config.el
+++ inits/linux-config.el
@@ -7,5 +7,6 @@
 (el-get-bundle! mozc
   :type http
   :url "https://mozc.googlecode.com/svn/trunk/src/unix/emacs/mozc.el"
-  (setq default-input-method "japanese-mozc"))
+  (setq default-input-method "japanese-mozc")
+  (global-set-key (kbd "s-SPC") 'toggle-input-method))
 (el-get-bundle! pos-tip)
