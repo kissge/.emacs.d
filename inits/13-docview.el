5ccec7594b1f3dc5a9f04257872622c647d7f664
kissge <mail@yo.eki.do>
2015-11-10 16:44:41 +0900

    Always auto-revert when opening PDF documents

--- inits/13-docview.el
+++ inits/13-docview.el
@@ -1,5 +1,6 @@
 (eval-when-compile (require 'doc-view))
 (add-hook 'doc-view-mode-hook
           (lambda ()
+            (auto-revert-mode)
             (define-key doc-view-mode-map (kbd "j") 'doc-view-next-page)
             (define-key doc-view-mode-map (kbd "k") 'doc-view-previous-page)))
