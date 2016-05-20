b1d2f5b2e8b1b7604a619179a4777c4a1b9f5a22
kissge <mail@yo.eki.do>
2016-05-21 00:27:49 +0900

    Use Python 3 for jedi

--- inits/15-python.el
+++ inits/15-python.el
@@ -2,4 +2,9 @@
   (add-hook 'python-mode-hook 'jedi:setup)
   (custom-set-variables '(jedi:complete-on-dot t)
                         '(jedi:tooltip-method nil))
-  (set-face-attribute 'jedi:highlight-function-argument nil :foreground "green"))
+  (set-face-attribute 'jedi:highlight-function-argument nil :foreground "green")
+  (if (executable-find "python3")
+      (custom-set-variables '(jedi:environment-root "jedi")
+                            '(jedi:environment-virtualenv
+                              (append python-environment-virtualenv
+                                      `("--python" ,(executable-find "python3")))))))
