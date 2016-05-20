e88a80983d32741f5fed7b4df36acade3ef706fe
kissge <mail@yo.eki.do>
2016-05-21 00:27:21 +0900

    Show eldoc-like arguments helper in python-mode

--- inits/15-python.el
+++ inits/15-python.el
@@ -1,3 +1,5 @@
 (el-get-bundle jedi
   (add-hook 'python-mode-hook 'jedi:setup)
-  (custom-set-variables '(jedi:complete-on-dot t)))
+  (custom-set-variables '(jedi:complete-on-dot t)
+                        '(jedi:tooltip-method nil))
+  (set-face-attribute 'jedi:highlight-function-argument nil :foreground "green"))
