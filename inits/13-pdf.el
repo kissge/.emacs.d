c490f22323b24a141fdc6fb5718e6d67ebbfc293
kissge <mail@yo.eki.do>
2017-02-09 21:44:57 +0900

    Install pdf-tools only when make is available

--- inits/13-pdf.el
+++ inits/13-pdf.el
@@ -1,10 +1,11 @@
-(el-get-bundle pdf-tools
-  (add-hook 'doc-view-mode-hook
-            (defun pdf-view-mode-initialize ()
-              (pdf-tools-install)
-              (remove-hook 'doc-view-mode-hook 'pdf-view-mode-initialize)))
-  (add-hook 'pdf-view-mode-hook
-            (defun pdf-view-mode-my-hook ()
-              (auto-revert-mode)
-              (local-set-key (kbd "j") 'pdf-view-next-page-command)
-              (local-set-key (kbd "k") 'pdf-view-previous-page-command))))
+(when (executable-find "make")
+  (el-get-bundle pdf-tools
+    (add-hook 'doc-view-mode-hook
+              (defun pdf-view-mode-initialize ()
+                (pdf-tools-install)
+                (remove-hook 'doc-view-mode-hook 'pdf-view-mode-initialize)))
+    (add-hook 'pdf-view-mode-hook
+              (defun pdf-view-mode-my-hook ()
+                (auto-revert-mode)
+                (local-set-key (kbd "j") 'pdf-view-next-page-command)
+                (local-set-key (kbd "k") 'pdf-view-previous-page-command)))))
