6e5e2c8e058a375ed9cac132d6486c95a8b4138d
kissge <mail@yo.eki.do>
2017-02-13 16:56:51 +0900

    Install pdf-tools only when autoreconf is available

--- inits/13-pdf.el
+++ inits/13-pdf.el
@@ -1,10 +1,11 @@
-(el-get-bundle pdf-tools
-  (autoload 'pdf-view-mode "pdf-view")
-  (setq auto-mode-alist (cons '("\\.pdf$" . pdf-view-mode) auto-mode-alist))
-  (with-eval-after-load-feature "pdf-tools"
-    (pdf-tools-install))
-  (add-hook 'pdf-view-mode-hook
-            (defun pdf-view-mode-my-hook ()
-              (auto-revert-mode)
-              (local-set-key (kbd "j") 'pdf-view-next-page-command)
-              (local-set-key (kbd "k") 'pdf-view-previous-page-command))))
+(when (executable-find "autoreconf")
+  (el-get-bundle pdf-tools
+    (add-hook 'doc-view-mode-hook
+              (defun pdf-view-mode-initialize ()
+                (pdf-tools-install)
+                (remove-hook 'doc-view-mode-hook 'pdf-view-mode-initialize)))
+    (add-hook 'pdf-view-mode-hook
+              (defun pdf-view-mode-my-hook ()
+                (auto-revert-mode)
+                (local-set-key (kbd "j") 'pdf-view-next-page-command)
+                (local-set-key (kbd "k") 'pdf-view-previous-page-command)))))
