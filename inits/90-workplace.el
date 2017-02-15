8e73282d44f520db8ab06c3a0e1fef1f4c229eaf
kissge <mail@yo.eki.do>
2017-02-15 16:02:32 +0900

    Add something

--- inits/90-workplace.el
+++ inits/90-workplace.el
@@ -1,3 +1,11 @@
 (when env-hikarie
   (setq-default tags-file-name "/eng4:TAGS")
 )
+
+(when env-ms
+  (global-set-key
+   (kbd "C-x ,")
+   (defun git-add-force-this-file ()
+     (interactive)
+     (shell-command (concat "git add -f \"" (buffer-file-name) "\""))))
+  )
