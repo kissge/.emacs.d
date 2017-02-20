a3fa3c0af9af27034a6a6a515b73a62f9153e66b
kissge <mail@yo.eki.do>
2017-02-20 16:03:16 +0900

    Refactor auto-mode-alist manipulation

--- inits/90-workplace.el
+++ inits/90-workplace.el
@@ -5,6 +5,12 @@
 (when env-ms
   (prefer-coding-system 'utf-8-dos)
   (custom-set-variables '(nxml-child-indent 4))
+  (setq auto-mode-alist
+      (append '(("\\.uix$" . xml-mode)
+                ("\\.dm$" . xml-mode)
+                ("\\.ds$" . xml-mode)
+                )
+              auto-mode-alist))
   (global-set-key
    (kbd "C-x ,")
    (defun git-add-force-this-file ()
