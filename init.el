1772768fb0506552c5cae483dc28c5cb677158f8
kissge <mail@yo.eki.do>
2017-03-30 17:02:45 +0900

    Silence Package.el

--- init.el
+++ init.el
@@ -23,3 +23,9 @@
   (custom-set-variables '(init-loader-show-log-after-init 'error-only)
                         '(init-loader-order 'platform-specific-first))
   (init-loader-load))
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+;; (package-initialize)
