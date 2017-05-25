a730923621c677ae58f34cfcf4e5972c4e9b68f4
kissge <mail@yo.eki.do>
2017-05-25 13:16:58 +0900

    Use cygwin binaries on Windows

--- inits/windows-config.el
+++ inits/windows-config.el
@@ -1,6 +1,8 @@
 (tool-bar-mode 0)
 (menu-bar-mode 0)
 
+(add-to-list 'exec-path "C:/cygwin64/bin")
+
 ;; workaround for Windows-style path separator specific problem
 ;; hint: locate-user-emacs-file, replace-match
 (advice-add 'locate-user-emacs-file :filter-return 'expand-file-name)
